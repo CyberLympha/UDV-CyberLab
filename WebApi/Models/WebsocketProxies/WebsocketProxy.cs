8405241b15f286ca7c516d29c7badb386f2fa387
daHil <ssocmain@gmail.com>
2024-04-25 00:57:18 +0500

    added entity for websocket proxy process and it's settings

---
+++ WebApi/Models/WebsocketProxies/WebsocketProxy.cs
@@ -0,0 +1,85 @@
+﻿using System.Diagnostics;
+
+namespace WebApi.Models.WebsocketProxies;
+
+/// <summary>
+/// Represents a WebSocket proxy that manages a Node.js process to handle WebSocket connections.
+/// </summary>
+public class WebsocketProxy
+{
+    private Process process;
+    private readonly string source;
+    private readonly string target;
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WebsocketProxy"/> class.
+    /// </summary>
+    /// <param name="source">The source address for the WebSocket proxy.</param>
+    /// <param name="target">The target address to which WebSocket messages will be forwarded.</param>
+    public WebsocketProxy(string source, string target)
+    {
+        this.source = source;
+        this.target = target;
+    }
+    
+    /// <summary>
+    /// Starts the WebSocket proxy using a Node.js script.
+    /// </summary>
+    /// <param name="nodeExePath">The path to the Node.js executable.</param>
+    /// <param name="scriptPath">The path to the Node.js script that implements the WebSocket proxy.</param>
+    /// <returns>True if the WebSocket proxy was successfully started; otherwise, false.</returns>
+    public bool Start(string nodeExePath, string scriptPath)
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = nodeExePath,
+            Arguments = $"{scriptPath} {source} {target}",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true
+        };
+
+        try
+        {
+            process = Process.Start(startInfo);
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+        
+
+        process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+        process.ErrorDataReceived += (sender, e) => Console.WriteLine($"Error: {e.Data}");
+        
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+
+        return true;
+    }
+
+    /// <summary>
+    /// Stops the WebSocket proxy process.
+    /// </summary>
+    /// <returns>True if the WebSocket proxy was successfully stopped or if no process is running; otherwise, false.</returns>
+    public bool Stop()
+    {
+        if (process is null)
+            return true;
+        
+        try
+        {
+            process.CancelErrorRead();
+            process.CancelOutputRead();
+            process.Kill();
+            process.Close();
+            
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+}
---
+++ WebApi/Models/WebsocketProxies/WebsocketProxySettings.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+
+namespace WebApi.Models.WebsocketProxies;
+
+/// <summary>
+/// Represents settings for the WebSocket proxy.
+/// </summary>
+public record WebsocketProxySettings
+{
+    /// <summary>
+    /// Gets or sets the WebSocket host address.
+    /// </summary>
+    [Required]
+    public required string WebsocketHost { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the starting port for Proxmox VNC connections.
+    /// </summary>
+    [Required]
+    public required int ProxmoxVncStartingPort { get; set; }
+}
