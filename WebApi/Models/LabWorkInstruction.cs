25e8b56170cb3a3d00446abea2c1f786d8a205ea
daHil <ssocmain@gmail.com>
2024-05-12 14:15:46 +0500

    added instruction model for lab work

---
+++ WebApi/Models/LabWorkInstruction.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Build.Framework;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApi.Models;
+
+/// <summary>
+/// Represents a set of instructions for a laboratory work.
+/// </summary>
+public record LabWorkInstruction
+{
+    /// <summary>
+    /// The unique identifier of the instruction.
+    /// </summary>
+    [BsonId]
+    [Required]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    
+    /// <summary>
+    /// The dictionary containing steps of the instruction.
+    /// Key: step number, Value: step ID.
+    /// </summary>
+    [Required]
+    public required Dictionary<string, string> Steps { get; set; }
+}
