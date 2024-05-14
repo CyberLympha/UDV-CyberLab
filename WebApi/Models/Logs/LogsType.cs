4c7ebe103aa263a6a69d96e0aaf17a6a9d9a016e
daHil <ssocmain@gmail.com>
2024-05-14 19:39:13 +0500

    added logs type enum

---
+++ WebApi/Models/Logs/LogsType.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi.Models.Logs;
+
+/// <summary>
+/// Enumerates the types of logs associated with a laboratory work instruction.
+/// </summary>
+public enum LogsType
+{
+    /// <summary>
+    /// Terminal logs.
+    /// </summary>
+    Terminal,
+    /// <summary>
+    /// Stall until other types of logs other than terminal are created.
+    /// </summary>
+    Empty
+}
