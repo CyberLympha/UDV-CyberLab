e3a02fd6737430f61a5a8bd0b0c25ebd4c6b3ad3
devilboi95 <turnickii.n@gmail>
2024-06-04 18:33:21 +0500

    refact lab

--- VirtualLab/Domain/Entities/Lab.cs
+++ VirtualLab/Domain/Entities/Lab.cs
@@ -8,18 +8,25 @@ public class Lab : IEntity<Guid>
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
-    public string Goal { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime DeadLine { get; set; }
+    public Guid CreatedBy { get; set; }
     public string Manual { get; set; }
-    
+    public bool IsOpened { get; set; }
+
+    public List<UserLab> UserLabs { get; set; }
 
     public static Lab From(LabCreateRequest request) // а что если это реализовывать на уровне контроллеера?
     {
         return new Lab()
         {
-            Goal = request.Goal,
+            Description= request.Goal,
             Id = Guid.NewGuid(),
             Manual = request.Manual,
             Name = request.Name,
+            CreatedBy = Guid.Parse("00000000-0000-0000-0000-000000000000"), //TODO: брать id из Identity
+            UserLabs = new List<UserLab>()
         };
     } 
 }
