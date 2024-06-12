548f6c77463fa6d190d69e2ed8ab88d907f65501
tereshkovyv <tyv2016@yandex.ru>
2024-06-12 20:57:12 +0500

    Added role to registration model

--- WebApi/Model/AuthModels/Requests/RegistrationRequest.cs
+++ WebApi/Model/AuthModels/Requests/RegistrationRequest.cs
@@ -6,6 +6,7 @@ public class RegistrationRequest
 {
     [Required] public string FirstName { get; set; } = null!;
     [Required] public string SecondName { get; set; } = null!;
+    [Required] public UserRole Role { get; set; } = UserRole.Anon;
     [Required] public string Email { get; set; } = null!;
     [Required] public string Password { get; set; } = null!;
 }
